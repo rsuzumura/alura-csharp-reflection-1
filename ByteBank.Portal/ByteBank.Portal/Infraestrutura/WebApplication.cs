2e17771d18846e50bbea167de74454ece07276e4
Ricardo Suzumura (Externo) <ricardo.suzumura@ext.dersa.sp.gov.br>
2019-05-21 07:18:41 -0300

    Desenvolvimento da aula 1:
    Criação de servidor web básico.

---
+++ ByteBank.Portal/ByteBank.Portal/Infraestrutura/WebApplication.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace ByteBank.Portal.Infraestrutura
+{
+    public class WebApplication
+    {
+        private readonly string[] _prefixos;
+        public WebApplication(string[] prefixos)
+        {
+            if (prefixos == null)
+                throw new ArgumentNullException(nameof(prefixos));
+            _prefixos = prefixos;
+        }
+
+        public void Iniciar()
+        {
+            var httpListener = new HttpListener();
+            foreach (var prefixo in _prefixos)
+            {
+                httpListener.Prefixes.Add(prefixo);
+            }
+            httpListener.Start();
+
+            var contexto = httpListener.GetContext();
+            var requisicao = contexto.Request;
+            var resposta = contexto.Response;
+            var respostaConteudo = "Hello World!!!";
+            var respostaConteudoBytes = Encoding.UTF8.GetBytes(respostaConteudo);
+            resposta.ContentType = "text/html; charset=utf-8";
+            resposta.StatusCode = (int)HttpStatusCode.OK;
+            resposta.ContentLength64 = respostaConteudoBytes.Length;
+            resposta.OutputStream.Write(respostaConteudoBytes, 0, respostaConteudoBytes.Length);
+            resposta.OutputStream.Close();
+            httpListener.Stop();
+        }
+    }
+}
---
+++ ByteBank.Portal/ByteBank.Portal/Program.cs
@@ -0,0 +1,14 @@
+﻿using ByteBank.Portal.Infraestrutura;
+
+namespace ByteBank.Portal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prefixos = new[] { "http://localhost:5341/" };
+            var webApplication = new WebApplication(prefixos);
+            webApplication.Iniciar();
+        }
+    }
+}
